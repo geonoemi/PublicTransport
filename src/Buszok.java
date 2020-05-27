4c987b9d5dfce294be3dfc7423cea796f63676b0
László Noémi <geonoemilaszlo@gmail.com>
2020-05-27 20:07:26 +0200

    buszok beolvasasa

--- src/Buszok.java
+++ src/Buszok.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Buszok extends Jarmu implements Fosszilis {
 
@@ -10,7 +14,30 @@ public class Buszok extends Jarmu implements Fosszilis {
 				javitasSzukseges, uzemanyagTipus);
 		this.rendszam = rendszam;		
 	}
-
-
+	public static void beolvas(String fajlnev){ //throws FileNotFoundException, IOException
+	try {
+		
+		FileReader olvaso=new FileReader(fajlnev);
+		BufferedReader buffer=new BufferedReader(olvaso);
+		String sor=null;
+		int i=0;
+		
+		while((sor=buffer.readLine())!=null) {
+			if (i>1) System.out.println(sor);
+			i++;
+		}
+		buffer.close();
 	
+		
+	}catch(FileNotFoundException e) {
+		System.out.println("A fájl nem található.");
+	}catch(IOException e) {
+		System.out.println("e.getMessage()");
+	}
+	}
+	public static void main(String[] args) {
+	
+		beolvas("buszok.txt");
+		
+	}	
 }
