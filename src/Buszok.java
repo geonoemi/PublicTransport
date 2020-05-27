fd4d90af7839e265798e514e2e4d14ad236e22ad
László Noémi <geonoemilaszlo@gmail.com>
2020-05-27 20:26:51 +0200

    buszok split

--- src/Buszok.java
+++ src/Buszok.java
@@ -23,7 +23,36 @@ public class Buszok extends Jarmu implements Fosszilis {
 		int i=0;
 		
 		while((sor=buffer.readLine())!=null) {
-			if (i>1) System.out.println(sor);
+			
+				String parts[] = sor.split(",");
+			
+				String[]vonalSzam=new String[sor.length()];
+				String[]vonalBetu=new String[sor.length()];
+				String[]irany=new String[sor.length()];
+				String[]csuklos=new String[sor.length()];
+				String[]alacsonyPadlos=new String[sor.length()];
+				String[]uzemeltetesiKoltseg=new String[sor.length()];
+				String[]uloHelyekSzama=new String[sor.length()];
+				String[]bicikliSzallitasiLehetoseg=new String[sor.length()];
+				String[]rokkantHelyekSzama=new String[sor.length()];
+				String[]javitasSzukseges=new String[sor.length()];
+				String[]uzemanyagTipus=new String[sor.length()];
+				
+				vonalSzam[i]=parts[0];
+				vonalBetu[i]=parts[1];
+				csuklos[i]=parts[2];
+				irany[i]=parts[3];
+				alacsonyPadlos[i]=parts[4];
+				uzemeltetesiKoltseg[i]=parts[5];
+				uloHelyekSzama[i]=parts[6];
+				bicikliSzallitasiLehetoseg[i]=parts[7];
+				rokkantHelyekSzama[i]=parts[8];
+				javitasSzukseges[i]=parts[9];
+				uzemanyagTipus[i]=parts[10];
+				
+			System.out.println(uzemanyagTipus[i]);
+			
+			
 			i++;
 		}
 		buffer.close();
