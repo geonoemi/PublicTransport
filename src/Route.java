cd4a5d414cdd9867e5d0cb68685e61cb21f3dc10
László Noémi <geonoemilaszlo@gmail.com>
2020-05-30 23:45:32 +0200

    Route departureTime without fileReader

--- src/Route.java
+++ src/Route.java
@@ -1,40 +1,48 @@
 
-public class Route {
+public class Route extends Vehicle{
 //járat
 
 	private String departureTime;
 		
-		public Route(String departureTime) {
-			
-			this.departureTime=departureTime;
-		}
+		public Route(int lineNum, String lineLetter, String way, boolean articulated, boolean lowFloor,
+						double operationCost, int numOfSeats, boolean bicycleTransportOpp, int numOfDisabledPlaces,
+						boolean needToRepair, String typeOfFuel, boolean hasWheel) {
+				super(lineNum, lineLetter, way, articulated, lowFloor, operationCost, numOfSeats, bicycleTransportOpp,
+						numOfDisabledPlaces, needToRepair, typeOfFuel, hasWheel);
+				this.departureTime=departureTime;
+	}
+
+		
 		public static DayType dayTypes;
 		//if vonalszam[i]==valami && vonalBetu.equals("valami") && dayTypes
-
+	//ide kell egy fájlbeolvasó az indulási idõkrõl
+		
+		
+		
 		public static void main(String[] args) {
 			
 			for (DayType dt : DayType.values()) {
-			    System.out.println("ENUM: "+ dt);
-			    if(	dt==DayType.WORKINGDAY) {
+			  //  System.out.println("ENUM: "+ dt);
+			    if(	dt==DayType.WORKINGDAY){
 			    	System.out.println("It's a working day! ");
-			    }else if(dt==DayType.NONWORKINGDAY) {
-			    	System.out.println("It's a non-working day! ");
-			    }else if(dt==DayType.DAYOFF) {
-			    	System.out.println("It's a DAYOFF! ");
-			    }else if(dt==DayType.WORKINGDAYSDURINGSCHOOLYEAR) {
-			    	System.out.println("It's a WORKING DAY DURING SCHOOL YEAR! ");
-			    }else if(dt==DayType.SUMMERBREAKWORKINGDAYS) {
-			    	System.out.println("It's a SUMMER BREAK WORKING DAY! ");
-			    }else if(dt==DayType.ONPUBLICHOLIDAYS) {
-			    	System.out.println("It's an ONPUBLICHOLIDAY! ");
-			    }else if(dt==DayType.DURINGSCHOOLHOLIDAYS) {
-			    	System.out.println("It's an DURINGSCHOOLHOLIDAYS! ");
-			    }else if(dt==DayType.DAILYEXCEPTHOLIDAY) {
-			    	System.out.println("It's an DAILYEXCEPTHOLIDAY! ");
-			    }else if(dt==DayType.DAILYEXCEPTONPUBLICHOLIDAYS) {
-			    	System.out.println("It's an DAILYEXCEPTONPUBLICHOLIDAYS! ");
+			    	//olvassa be a munkanapos indulási idõket tartalmazó file-t
+			    	for(Vehicle v:vehicles) {
+			    		if(v.lineNum==2 && v.way.contentEquals("ODA")) { //mindenvonal mindkét irányára
+			    			//olvassa be a 2-es oda járat indulási idõket tartalmazó filet
+			    			//this.departureTime=munkanap sorainak értékei tömbbe
+						 }
+			    	}
+			    }else if(dt==DayType.NONWORKINGDAY) { System.out.println("It's a non-working day! ");
+			    }else if(dt==DayType.DAYOFF) { System.out.println("It's a DAYOFF! ");
+			    }else if(dt==DayType.WORKINGDAYSDURINGSCHOOLYEAR) {	System.out.println("It's a WORKING DAY DURING SCHOOL YEAR! ");
+			    }else if(dt==DayType.SUMMERBREAKWORKINGDAYS) {System.out.println("It's a SUMMER BREAK WORKING DAY! ");
+			    }else if(dt==DayType.ONPUBLICHOLIDAYS) { System.out.println("It's an ONPUBLICHOLIDAY! ");
+			    }else if(dt==DayType.DURINGSCHOOLHOLIDAYS) { System.out.println("It's a DURINGSCHOOLHOLIDAYS! ");
+			    }else if(dt==DayType.DAILYEXCEPTHOLIDAY) { System.out.println("It's a DAILYEXCEPTHOLIDAY! ");
+			    }else if(dt==DayType.DAILYEXCEPTONPUBLICHOLIDAYS) {	System.out.println("It's a DAILYEXCEPTONPUBLICHOLIDAYS! ");
 			    }               
 			}
+		
 		}
 	}
 
