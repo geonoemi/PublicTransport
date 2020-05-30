37688292dc56647280f1611cc778b9e108a00c1b
László Noémi <geonoemilaszlo@gmail.com>
2020-05-31 00:10:32 +0200

    Route departureTime with fileReader

--- src/Route.java
+++ src/Route.java
@@ -1,38 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Route extends Vehicle{
 //járat
 
-	private String departureTime;
+	private static String []departureTimes;
 		
 		public Route(int lineNum, String lineLetter, String way, boolean articulated, boolean lowFloor,
 						double operationCost, int numOfSeats, boolean bicycleTransportOpp, int numOfDisabledPlaces,
 						boolean needToRepair, String typeOfFuel, boolean hasWheel) {
 				super(lineNum, lineLetter, way, articulated, lowFloor, operationCost, numOfSeats, bicycleTransportOpp,
 						numOfDisabledPlaces, needToRepair, typeOfFuel, hasWheel);
-				this.departureTime=departureTime;
-	}
+				}
 
 		
 		public static DayType dayTypes;
 		//if vonalszam[i]==valami && vonalBetu.equals("valami") && dayTypes
 	//ide kell egy fájlbeolvasó az indulási idõkrõl
 		
-		
-		
+		public static void readIn(String fileName){ 
+			try {
+				
+				FileReader reader=new FileReader(fileName);
+				BufferedReader buffer=new BufferedReader(reader);
+				String queue=null;
+				int i=0;
+				
+				while((queue=buffer.readLine())!=null) {
+					
+					departureTimes=new String[queue.length()];
+					departureTimes[i]=queue;
+					System.out.println(departureTimes[i]);
+					i++;
+					
+				}buffer.close();
+				
+			}catch(FileNotFoundException e) {
+				System.out.println("File not found.");
+			}catch(IOException e) {
+				System.out.println("e.getMessage()");
+			}		
+	}	
+	static DayType dt=DayType.WORKINGDAY;
+	
 		public static void main(String[] args) {
-			
-			for (DayType dt : DayType.values()) {
+			readIn("working day departure times for 2 ODA.txt");
+
+			//for (DayType dt : DayType.values()) {
 			  //  System.out.println("ENUM: "+ dt);
 			    if(	dt==DayType.WORKINGDAY){
 			    	System.out.println("It's a working day! ");
 			    	//olvassa be a munkanapos indulási idõket tartalmazó file-t
 			    	for(Vehicle v:vehicles) {
-			    		if(v.lineNum==2 && v.way.contentEquals("ODA")) { //mindenvonal mindkét irányára
-			    			//olvassa be a 2-es oda járat indulási idõket tartalmazó filet
-			    			//this.departureTime=munkanap sorainak értékei tömbbe
+			    		//if(v.lineNum==2 && v.way.contentEquals("ODA")) { //mindenvonal mindkét irányára
+			    //			readIn("working day departure times for 2 ODA.txt");
 						 }
 			    	}
-			    }else if(dt==DayType.NONWORKINGDAY) { System.out.println("It's a non-working day! ");
+			/*    }else if(dt==DayType.NONWORKINGDAY) { System.out.println("It's a non-working day! ");
 			    }else if(dt==DayType.DAYOFF) { System.out.println("It's a DAYOFF! ");
 			    }else if(dt==DayType.WORKINGDAYSDURINGSCHOOLYEAR) {	System.out.println("It's a WORKING DAY DURING SCHOOL YEAR! ");
 			    }else if(dt==DayType.SUMMERBREAKWORKINGDAYS) {System.out.println("It's a SUMMER BREAK WORKING DAY! ");
@@ -40,9 +66,10 @@ public class Route extends Vehicle{
 			    }else if(dt==DayType.DURINGSCHOOLHOLIDAYS) { System.out.println("It's a DURINGSCHOOLHOLIDAYS! ");
 			    }else if(dt==DayType.DAILYEXCEPTHOLIDAY) { System.out.println("It's a DAILYEXCEPTHOLIDAY! ");
 			    }else if(dt==DayType.DAILYEXCEPTONPUBLICHOLIDAYS) {	System.out.println("It's a DAILYEXCEPTONPUBLICHOLIDAYS! ");
-			    }               
+			    }   */            
 			}
 		
 		}
-	}
+	
+//	}
 
