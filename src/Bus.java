9b9d75ec42e25f9e8ecf21aba4ad7330d75782fa
László Noémi <geonoemilaszlo@gmail.com>
2020-06-04 09:17:50 +0200

    main methods into Run class

--- src/Bus.java
+++ src/Bus.java
@@ -13,38 +13,6 @@ public class Bus extends Vehicle implements Fossil {
 			super( lineNum, lineLetter, way, articulated, lowFloor, operationCost, numOfSeats, bicycleTransportOpp,
 					numOfDisabledPlaces, needToRepair, typeOfFuel, hasWheel);
 		}
-	
-		
-		public static void main(String[] args) {
-		
-			readIn("classes files\\buses.txt");
-			
-			for(Vehicle vehicle: vehicles) {
-				if(vehicle.getClass().equals(Bus.class)) {
-					System.out.println("This is a bus.");
-				}
-				if(vehicle.lowFloor && vehicle.numOfDisabledPlaces>=1) {
-					//Vehicle wheelchairAccessibleVehicle=new WheelChairAccessible(vehicle.lineNum, vehicle.lineLetter, vehicle.way, vehicle.articulated, vehicle.lowFloor,
-					//	vehicle.operationCost,  vehicle.numOfSeats, vehicle.bicycleTransportOpp, vehicle.numOfDisabledPlaces, vehicle. needToRepair, vehicle.typeOfFuel, vehicle.hasWheel);
-					wheelChairAccessibleVehicles.add((WheelChairAccessible) vehicle); // TODO: may cause ClassCastEx.
-				}
-					
-				if(vehicle.needToRepair) {
-					toService.add(vehicle);
-				}
-				
-				if(vehicle.bicycleTransportOpp) {
-					bicycleVehicles.add(vehicle);
-				}
-				
-				if(vehicle.typeOfFuel.equals("petrol") || vehicle.typeOfFuel.equals("diesel oil")) {
-					fossilVehicles.add(vehicle);
-				}else {
-					electricVehicles.add(vehicle);
-				} 
-			}
-		}
-
-	}
+}
 
 
