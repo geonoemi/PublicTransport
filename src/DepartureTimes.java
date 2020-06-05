0860c02b894e277f780ea50d5a3c435745e2c5c7
László Noémi <geonoemilaszlo@gmail.com>
2020-06-05 15:44:48 +0200

    readIn txt debug

--- src/DepartureTimes.java
+++ src/DepartureTimes.java
@@ -19,7 +19,7 @@ public class DepartureTimes extends Route {
         ArrayList<String> departureTimesList = new ArrayList<String>(Arrays.asList(directory.list()));
 
         for(String list: departureTimesList) {
-        	if(list.contains(choice)) {
+        	if(list.equals(choice+".txt")) {
         	Route.readIn("departure times\\"+list);
         	}
         }
